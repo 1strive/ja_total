5c985fe584be347d81ff490c07e567c658cd9e29
ja <pride.ja0707@gmail.com>
2022-05-12 16:18:41 +0800

    feat: 🎸 222

--- test/test.js
+++ test/test.js
@@ -1,14 +1,5 @@
-const a = 2
-
-function s() {
-    switch (a) {
-        case 2:
-            console.log(2);
-            return 2
-        // break;
-        case (2 || 3):
-            console.log(3);
-        default:
-            console.log(4);
-    }
+const obj = {
+    a: 2
 }
+
+console.log(ans);
