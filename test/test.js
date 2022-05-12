ed6524600ab565504914886a51ebd022c417b3cb
ja <pride.ja0707@gmail.com>
2022-05-12 09:32:01 +0800

    feat: 🎸 sohu

--- test/test.js
+++ test/test.js
@@ -1,3 +1,14 @@
-const a = 1
-console.log(obj);
-obj.a = 2
+const a = 2
+
+function s() {
+    switch (a) {
+        case 2:
+            console.log(2);
+            return 2
+        // break;
+        case (2 || 3):
+            console.log(3);
+        default:
+            console.log(4);
+    }
+}
